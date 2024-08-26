7dae8778c343e0b8fd375f6597ba6cb2a69825fa
viarony <victorpaularony>
2024-08-26 15:43:17 +0300

    FILE: a function to concatinate two strings of arguments

--- concat-str.js
+++ concat-str.js
@@ -1,7 +1,13 @@
 
-const args1 = "victor "
-var args2 = "paul" 
+const args1 = 2
+var args2 = 1 
 function concatStr(args1, args2){
+    if (typeof(args1) ==='number'){
+        args1 = args1.toString()
+    }
+    if (typeof(args2) === 'number'){
+    args2 = args2.toString()
+    }
     return args1 + args2
 }
-console.log(concatStr(args1,args2))
+console.log(concatStr(args1, args2))
