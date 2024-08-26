3bb3cfab6df754bf4d92e1afb2bb24dab25ecf69
viarony <victorpaularony>
2024-08-26 20:52:30 +0300

    FILE: function to capitalize, lower, split and upper

--- method-man.js
+++ method-man.js
@@ -14,11 +14,12 @@ function whisper(str){
     return `*${str.toLowerCase()}*`;
 }
 function capitalize(str){
-    const s = str.split(" ")
-    for (let i= 0; i< s.length;i++){
-        s[i] = s[i][0].toUpperCase() + s[i].substr(1).toLowerCase()
-    }
-   return s.join(" ")
+//     const s = str.split(" ")
+//     for (let i= 0; i< s.length;i++){
+//         s[i] = s[i][0].toUpperCase() + s[i].substr(1).toLowerCase()
+//     }
+//    return s.join(" ")
+return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
    
 }
 
