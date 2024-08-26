1263fa1b7069a9a9997724a95358183f25f02004
viarony <victorpaularony>
2024-08-26 20:23:40 +0300

    FILE: function to capitalize, lower, split and upper

---
+++ method-man.js
@@ -0,0 +1,27 @@
+const str = "The quick brown fox jumped over the lazy dog"
+const arr = ["the", "quick", "brown", "fox", "jumped"]
+const str1 = "THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG"
+
+function word(){
+    return str.split(' ')
+}
+function sentence(){
+    return arr.join(' ')
+}
+
+function yell(){
+    return str.toUpperCase()
+}
+
+function whisper(){
+    return str1.toLowerCase()
+}
+function capitalize(str2){
+    return str2.charAt(0).toUpperCase() + str2.slice(1)
+}
+
+console.log(word())
+console.log(sentence())
+console.log(yell())
+console.log(whisper())
+console.log(capitalize('the quick brown fox jumped over the lazy dog'))
