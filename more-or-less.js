6afe0624036576ed7c44b7d59ce96d0781baa5bb
viarony <victorpaularony>
2024-08-26 14:43:41 +0300

    FILE: functions with arguments to add and subtract

--- more-or-less.js
+++ more-or-less.js
@@ -9,9 +9,9 @@ function add(num, num1){
     return num + num1
 }
 function sub(num, num1){
-    return num1 - num
+    return num - num1
 }
-console.log(sub(10,20))
+console.log(sub(3,10))
 console.log(add(10,20))
 console.log(less(10))
 console.log(more(10))
