f8f6e28adec211db13ddddefe88c509b40430a4b
viarony <victorpaularony>
2024-08-26 14:10:14 +0300

    FILE: mulability file to manupulate the object

--- mutability.js
+++ mutability.js
@@ -1,25 +1,11 @@
-const person = {
-    name: 'person',
-    age: 10,
-    country: 'US',
-  }
+
 
 const clone1 = {...person }
 const clone2 = Object.assign({}, person)
-const sameperson = {...person}
-
-clone1.name = 'clone1'
-clone2.name = 'clone2'
-sameperson.name = 'sameperson'
+const sameperson = person
 
 
 person.age ++
-sameperson.age ++
-sameperson.country = 'FR'
  person.country = 'FR'
 
 
-console.log(person)
-console.log(clone1)
-console.log(clone2)
-console.log(sameperson)
