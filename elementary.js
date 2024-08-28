d1d5e22263aa64dc2eb48ae2eda167b3f5e6b760
viarony <victorpaularony>
2024-08-28 12:27:58 +0300

    FILE: fuction to add, mult and modulus

--- elementary.js
+++ elementary.js
@@ -1,15 +1,16 @@
 const a = 10
-const b = 20
+const b = 30
 function multipy(){
     return a* b
 }
-function add(){
-    return a+b
+function didide(){
+    return a/b
 }
 function modulo(){
     return a%b
 }
 
-console.log(add())
+
 console.log(multipy())
+console.log(didide())
 console.log(modulo())
