d822eaea27521ac0b56ff5457c8a460a1b55ffe2
viarony <victorpaularony>
2024-08-28 11:48:35 +0300

    FILE: fuction to repeat a string

--- repeat.js
+++ repeat.js
@@ -1,9 +1,12 @@
 function repeat(str, num){
     let res = ''
+   if (num < 0){
+        throw new RangeError('out of renge')
+    } 
     for (let i= 0; i < num; i++){
         res += str
     }
     return res
 }
 
-console.log(repeat("hello", 1))
+console.log(repeat("hello", -1))
