a01aa8256052dcb5e70e011447f3fefa16382fac
viarony <victorpaularony>
2024-08-28 23:50:30 +0300

    FILE: function that return frist and second characters

---
+++ keep-cut.js
@@ -0,0 +1,63 @@
+//const str = 'victor paul'
+function cutFirst(str){
+    let res = ''
+    for (let i = 2; i < str.length;i++){
+         res += str[i]
+    }
+    return res
+}
+
+function cutLast(str){
+    let res = ''
+    for (let i = 0; i < str.length-2; i++){
+        res += str[i]
+    }
+    return res
+}
+
+function cutFirstLast(str){
+    let res = ''
+    for (let i = 2; i < str.length-2; i++){
+        res += str[i]
+    }
+    return res
+}
+
+function keepFirst(str){
+    let res = ''
+    for (let i = 0;i< 2 && i < str.length; i++){
+        res += str[i]
+    }
+    return res
+}
+
+function keeoLast(str){
+    let res = ''
+    for (let i = str.length-2; i < str.length; i++){
+        res += str[i]
+    }
+    return res
+}
+ function keepFirstLast(str){
+    let res1 = ''
+    let res2 = ''
+
+    for (let i = 0;i< 2 && i < str.length; i++){
+        res1 += str[i]
+    }
+
+    for (let i = str.length-2; i < str.length; i++){
+        res2 += str[i]
+    }
+    return res1.concat('', res2)
+
+ }
+
+
+
+// console.log(cutFirst(str))
+// console.log(cutLast(str))
+// console.log(cutFirstLast(str))
+// console.log(keepFirst(str))
+// console.log(keeoLast(str))
+// console.log(keepFirstLast(str))
