9f8a77a8e453800c01682ec96628804eedc81b91
viarony <victorpaularony>
2024-08-29 01:47:56 +0300

    FILE: function that works as math method

--- cut-corners.js
+++ cut-corners.js
@@ -11,6 +11,7 @@ function ceil(num){
             return num
         }
     }
+    
   
     if (num > 0){
         return parseInt(num + 1)
@@ -42,8 +43,8 @@ function trunc(num){
 
 
 
-const nums = [3.7, -3.7, 3.1, -3.1]
-console.log(nums.map(round))
-console.log(nums.map(floor))
-console.log(nums.map(trunc))
-console.log(nums.map(ceil))
+// const nums = [3.7, -3.7, 3.1, -3.1]
+// console.log(nums.map(round))
+// console.log(nums.map(floor))
+// console.log(nums.map(trunc))
+// console.log(nums.map(ceil))
