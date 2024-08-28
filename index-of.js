f09c2d3c852b7109c89168212c61490a2a62209b
viarony <victorpaularony>
2024-08-28 17:25:05 +0300

    file: fuction to reeturn index of array elements

--- index-of.js
+++ index-of.js
@@ -1,7 +1,7 @@
-function indexOf(arr, val) {
+function indexOf(arr, val, from = 0) {
     //return arr.indexOf(val)
     
-    for (let i = 0; i < arr.length; i++){
+    for (let i = from; i < arr.length; i++){
         if (arr[i] === val){
            return i
         }
@@ -23,10 +23,12 @@ function includes(arr, val){
     //return arr.includes(val)
     for (let i = 0;i < arr.length; i++){
         if (arr[i] === val){
-            return i
+            return true
         }
     }
+    return false
 }
-console.log(indexOf([1, 2, 3, 4, 5], 4))
+let t = 'test'
+console.log(indexOf([t, 0, 0, t], t, 1))
 console.log(lastIndexOf([1, 2, 3, 4, 5, 6]))
 console.log(includes([1, 2, 3, 4, 5], 3))
