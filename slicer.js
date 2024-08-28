c4990882bdc21ece6f8b808519b351cf7947ba99
viarony <victorpaularony>
2024-08-28 21:27:39 +0300

    file: function to slice using push method

--- slicer.js
+++ slicer.js
@@ -1,6 +1,9 @@
-const arr = ["0", "1", "2", "3", "4", "5"]
+// const arr = 'abcdef'
 function slice(str, start, end){
     const res = []
+    if (end === undefined){
+        end = str.length
+    }
     for(let i = start; i < end && i < str.length; i++){
         res.push(str[i])
     }
@@ -8,4 +11,4 @@ function slice(str, start, end){
 
     return res
 }
-console.log(slice(arr,2,6))
+// console.log(slice(arr,2))
