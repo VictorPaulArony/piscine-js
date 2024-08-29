2a6fc142da5a440842d344f969054c9f59c1ad90
viarony <victorpaularony>
2024-08-29 17:59:49 +0300

    FILE: function that split and join

--- unbreakable.js
+++ unbreakable.js
@@ -1,8 +1,8 @@
-function split(str){
+function split(str, sep){
     let res = []
     let word = ''
     for (let i = 0; i < str.length; i++){
-        if (str[i] !== ' '){
+        if (str[i] !== sep){
             word += str[i]
         }else if (word !== ''){
             res.push(word)
@@ -14,11 +14,11 @@ function split(str){
     }
     return res
 }
-function join(arr){
+function join(arr , sep){
     let res = ""
     for (let i = 0; i < arr.length; i++){
     if(i > 0){
-        res += ' '
+        res += sep
     }
         res += arr[i]
     }
@@ -29,3 +29,8 @@ function join(arr){
 
 // console.log(split('hello world'))
 // console.log(join(['hello', 'world']))
+
+// console.log(split('hello,world,this,is,a,test', ',')); // ['hello', 'world', 'this', 'is', 'a', 'test']
+// console.log(join(['hello', 'world', 'this', 'is', 'a', 'test'], '-')); // 'hello-world-this-is-a-test'
+// console.log(split('one-two-three', '-')); // ['one', 'two', 'three']
+// console.log(join(['one', 'two', 'three'], '+')); // 'one+two+three'
