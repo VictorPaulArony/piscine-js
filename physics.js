0d84755e291673cd008e0a5e12ed2e7e0314f7d8
viarony <victorpaularony>
2024-08-29 09:25:26 +0300

    FILE: function to calculate the accelaration

--- physics.js
+++ physics.js
@@ -1,16 +1,22 @@
 
 function getAcceleration(s){
-    let a = 0
-    // let f = 0, t = 0, d = 0, m = 0,  Δv =  0, Δt =  0
-    if (s.f !== undefined && s.m !== undefined){
-        a = s.f/s.m
-    }else if (s.Δv !== undefined % s.Δt != undefined){
-         a = s.Δv/s.Δt
-    }
+   
     if (s.d !== undefined && s.t !== undefined){
         
-         a = (2*s.d)/(s.t*s.t)
+       return (2*s.d)/(s.t*s.t)
+   }else if (s.f !== undefined && s.m !== undefined){
+       return s.f/s.m
+    }else if (s.Δv !== undefined && s.Δt != undefined){
+       return s.Δv/s.Δt
+    }else {
+       return "impossible"
     }
-   
-    return a
 }
+// console.log(getAcceleration({
+//     // f: 10,
+//     // m: 5,
+//     // // Δv: 100,
+//     // Δt: 50,
+//     // t:1,
+//     d: 10
+//   }))
