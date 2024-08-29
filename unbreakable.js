ec5543016678d732eafe70672f36f6d1a2e27cb2
viarony <victorpaularony>
2024-08-29 18:14:01 +0300

    FILE: function that split and join

--- unbreakable.js
+++ unbreakable.js
@@ -1,36 +1,49 @@
-function split(str, sep){
-    let res = []
-    let word = ''
-    for (let i = 0; i < str.length; i++){
-        if (str[i] !== sep){
-            word += str[i]
-        }else if (word !== ''){
-            res.push(word)
-            word = ''
+// Custom split function to handle multi-character delimiters
+function split(str, sep) {
+    const res = [];
+    let word = '';
+    let i = 0;
+  
+    while (i < str.length) {
+      // Check if the substring matches the delimiter
+      if (str.slice(i, i + sep.length) === sep) {
+        if (word !== '') {
+          res.push(word); // Push the accumulated word to result
+          word = '';
         }
+        i += sep.length; // Skip over the length of the delimiter
+      } else {
+        word += str[i]; // Accumulate characters
+        i++;
+      }
     }
-    if (word !== ''){
-        res.push(word)
+  
+    // Push the last accumulated word if not empty
+    if (word !== '') {
+      res.push(word);
     }
-    return res
-}
-function join(arr , sep){
-    let res = ""
-    for (let i = 0; i < arr.length; i++){
-    if(i > 0){
-        res += sep
+  
+    return res;
+  }
+  
+  // Custom join function that remains the same as it correctly handles separators
+  function join(arr, sep) {
+    let res = '';
+    for (let i = 0; i < arr.length; i++) {
+      if (i > 0) {
+        res += sep;
+      }
+      res += arr[i];
     }
-        res += arr[i]
-    }
-    return res
-}
-
-
-
-// console.log(split('hello world'))
-// console.log(join(['hello', 'world']))
-
-// console.log(split('hello,world,this,is,a,test', ',')); // ['hello', 'world', 'this', 'is', 'a', 'test']
-// console.log(join(['hello', 'world', 'this', 'is', 'a', 'test'], '-')); // 'hello-world-this-is-a-test'
-// console.log(split('one-two-three', '-')); // ['one', 'two', 'three']
-// console.log(join(['one', 'two', 'three'], '+')); // 'one+two+three'
+    return res;
+  }
+  
+//   // Test cases
+//   console.log(split('hello world', ' ')); // ['hello', 'world']
+//   console.log(join(['hello', 'world'], ' ')); // 'hello world'
+//   console.log(split('hello,world,this,is,a,test', ',')); // ['hello', 'world', 'this', 'is', 'a', 'test']
+//   console.log(join(['hello', 'world', 'this', 'is', 'a', 'test'], '-')); // 'hello-world-this-is-a-test'
+//   console.log(split('one-two-three', '-')); // ['one', 'two', 'three']
+//   console.log(join(['one', 'two', 'three'], '+')); // 'one+two+three'
+//   console.log(split('ggg - ddd - b', ' - ')); // ['ggg', 'ddd', 'b']
+  
