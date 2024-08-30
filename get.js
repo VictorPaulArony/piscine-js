a7802189aeb575f7084c85c88b23bf9fd53b040d
viarony <victorpaularony>
2024-08-30 17:53:40 +0300

    FILE: function get that fetch the values of object entity

--- get.js
+++ get.js
@@ -1,7 +1,7 @@
 
-const src = { nested: { key: 'peekaboo',city: 'New York',
-zip: '10001' } }
-const path = 'nested.city'
+// const src = { nested: { key: 'peekaboo',city: 'New York',
+// zip: '10001' } }
+// const path = 'nested.city'
 function get(src, path){
     const pathArr = path.split('.')
     let res = src
@@ -10,4 +10,9 @@ function get(src, path){
     }
     return res ? res : undefined
 }
+/*
+Split the path string into parts to navigate through the object.
+Iterate over these parts, drilling down into the object step by step.
+Return the value if the path exists, or undefined if it doesn't.
+*/
 // console.log(get(src, path))
