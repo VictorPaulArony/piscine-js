753bd1d6e9835d6f22f81a48dc83b43e27b7c846
viarony <victorpaularony>
2024-08-30 18:35:54 +0300

    FILE: function to drown a star triangle

--- triangle.js
+++ triangle.js
@@ -1,10 +1,12 @@
 function triangle(str, num ){
     let res = ''
     for (let i = 0; i < num; i++){
-        res += str.repeat(i + 1) + '\n'
-       
+        res += str.repeat(i + 1) 
+        if (i !== num - 1){
+            res += '\n'
+        }
     }
-    return res
+    return res 
 }
 // function 
 console.log(triangle('*', 5))
