ff79f470c0f05bf7ac850a23c3a2248f03984be2
viarony <victorpaularony>
2024-08-30 18:58:35 +0300

    FILE: function to drow a trinange

---
+++ pyramid.js
@@ -0,0 +1,33 @@
+// function pyramid(str, num){
+//     let res = ''
+//   for (let i = 0; i < num; i++){
+//         res += ' '.repeat(num - i - 1) + str.repeat(i * 2 + 1) 
+    
+//     if  (i !== num - 1){
+//         res += '\n'
+//     }
+// }
+//     return res 
+// }
+// console.log(pyramid('*', 5))
+
+function pyramid(str, num) {
+    let res = ''
+
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num - i - 1; j++) {
+            res += ' '
+        }
+
+        for (let k = 0; k < i * 2 + 1; k++) {
+            res += str
+        }
+        if (i !== num - 1) {
+            res += '\n'
+        }
+    }
+
+    return res
+}
+
+console.log(pyramid('*', 10))
