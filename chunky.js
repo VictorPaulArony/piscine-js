a4bb6f28b09403f4047c6b691e078c90a78350ed
viarony <victorpaularony>
2024-08-30 15:00:48 +0300

    FILE: function that act like chunk

--- chunky.js
+++ chunky.js
@@ -1,35 +1,21 @@
-// function chunk(arr, int){
-//     let res = []
-//     let temp = []
-//     for (let i = 0; i < arr.length; i++){
-//         temp.push(arr[i])
-//         if (temp.length === int){
-//             res.push(temp)
-//             temp = []
-//         }
-//     }
-//     if (temp.length > 0){
-//         res.push(temp)
-//     }
-//     return res
-// }
-
-function chunk(sl, num){
+function chunk(arr, int){
     let res = []
-    let sm = []
-    for (let i = 0; i < sl.length;i++){
-            sm.push([i])
-        if (sm.length === num){
-            res.push(sm)
-            sm = []
+    let temp = []
+    for (let i = 0; i < arr.length; i++){
+        temp.push(arr[i])
+        if (temp.length === int){
+            res.push(temp)
+            temp = []
         }
     }
-    if (sm.length > 0){
-        res.push(sm)
+    if (temp.length > 0){
+        res.push(temp)
     }
     return res
 }
 
 
 
-console.log(chunk([1,2,3,4,5,6,7,8,9,10], 3))
+
+
+// console.log(chunk([1,2,3,4,5,6,7,8,9,10], 3))
