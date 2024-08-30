acfe0efb316b2f75385d6fc3dd35a6ca9fc2ba68
viarony <victorpaularony>
2024-08-30 18:07:57 +0300

    FILE: function get that fetch the values of object entity

--- get.js
+++ get.js
@@ -2,14 +2,13 @@
 // const src = { nested: { key: 'peekaboo',city: 'New York',
 // zip: '10001' } }
 // const path = 'nested.city'
-export const tests = [];
-const t = (f) => tests.push(f);
-
-// Define the vowels regular expression
-const vowels = /[aeiou]/i;
-
-function vowelDots(str) {
-    return str.replace(vowels, '$&.');
+function get(src, path){
+    const pathArr = path.split('.')
+    let res = src
+    for(let i = 0; i < pathArr.length; i++){
+        res = res[pathArr[i]]
+    }
+    return res ? res : undefined
 }
 /*
 Split the path string into parts to navigate through the object.
