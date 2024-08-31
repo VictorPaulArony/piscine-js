9cfc2fea5cac3296b40fede6284137c72374c4ea
viarony <victorpaularony>
2024-08-31 12:15:50 +0300

    FILE: function that adds dots when vowels are encounted

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,20 +1,14 @@
-export const tests = [];
-const t = (f) => tests.push(f);
-
-// Define the vowels regular expression
-const vowels = /[aeiou]/i;
 
 function vowelDots(str) {
-    return str.replace(vowels, '$&.');
+    const vowels = /[aeiou]/i;
+    let res = '';
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.test(str[i])) {
+            res += '.';
+        } else {
+            res += str[i];
+        }
+    }
 }
 
-/*
-Regular Expression: The correct regular expression /[aeiou]/g matches any of the vowels (a, e, i, o, u) in the string.
 
-String Replacement:
-
-The replace() method is used to replace each matched vowel.
-The '$&.' in the replace() method means "take the matched vowel ($&) and add a dot after it".
-
-console.log(vowelDots('hello world'))
-*/
