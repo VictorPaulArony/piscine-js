a7358a72b8d57cb1ab972fb3554504b4be0a4450
viarony <victorpaularony>
2024-08-31 13:18:12 +0300

    FILE: function to confirm if argument matches two regular expresions

--- same-amount.js
+++ same-amount.js
@@ -1,10 +1,13 @@
 function sameAmount(args, RegExp1, RegExp2){
+    const match1 = args.match(RegExp1) || []
+    const match2 = args.match(RegExp2) || []
    
-    if(RegExp1.test(args) === RegExp2.test(args)){
+    if(match1.length === match2.length){
         return true
     } else {
         return false
     }
 }
 // return RegExp1.test(args) === RegExp2.test(args)
-// console.log(sameAmount('12345', /[54321]/g, /[12345]/g))
+console.log(sameAmount('12345', /[54321]/g, /[12345]/g))
+
