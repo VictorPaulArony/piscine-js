835da2798d1298217f56d8351bd1a27dfd4a6f2c
viarony <victorpaularony>
2024-08-31 12:30:12 +0300

    FILE: function that adds dots when vowels are encounted

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,6 +1,6 @@
-
+const vowels = /[aeiou]/gi
 function vowelDots(str) {
-    const vowels = /[aeiou]/gi
+    
     let res = ''
     for (let i = 0; i < str.length; i++) {
         if (vowels.test(str[i])) {
