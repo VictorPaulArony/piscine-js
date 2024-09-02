166d57e1865830a6c281286827369ecdd622b177
viarony <victorpaularony>
2024-09-02 22:19:26 +0300

    FILE: functions that validate and check the dates

--- date-is.js
+++ date-is.js
@@ -4,18 +4,29 @@ function isValid(date) {
            (typeof date === 'number' && !isNaN(date) && date >= 0);
 }
 
+function toDate(date) {
+    // Convert a timestamp to a Date object if it's a number
+    return (date instanceof Date) ? date : new Date(date);
+}
+
 function isAfter(date1, date2) {
-    return isValid(date1) && isValid(date2) && date1.getTime() > date2.getTime();
+    const d1 = toDate(date1);
+    const d2 = toDate(date2);
+    return isValid(d1) && isValid(d2) && d1.getTime() > d2.getTime();
 }
 
 function isBefore(date1, date2) {
-    return isValid(date1) && isValid(date2) && date2.getTime() > date1.getTime();
+    const d1 = toDate(date1);
+    const d2 = toDate(date2);
+    return isValid(d1) && isValid(d2) && d2.getTime() > d1.getTime();
 }
 
 function isFuture(date) {
-    return isValid(date) && date.getTime() > Date.now();
+    const d = toDate(date);
+    return isValid(d) && d.getTime() > Date.now();
 }
 
 function isPast(date) {
-    return isValid(date) && date.getTime() < Date.now();
+    const d = toDate(date);
+    return isValid(d) && d.getTime() < Date.now();
 }
