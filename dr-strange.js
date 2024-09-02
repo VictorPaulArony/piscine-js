1ca34e6fc41875607e5b3cccdaf0c554a476deeb
viarony <victorpaularony>
2024-09-02 20:50:26 +0300

    FILE: function to use the epoch and dates

--- dr-strange.js
+++ dr-strange.js
@@ -1,27 +1,29 @@
-function addWeek (date){
-    const epoch = new Date('0001-01-01T00:00:00Z');
-    const diffinTime = date - epoch
-    const diffinDays = Math.floor(diffinTime / (1000 * 360 * 24))
+function addWeek(date) {
+    const daysOfWeek = [
+      'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+      'secondMonday', 'secondTuesday', 'secondWednesday', 'secondThursday', 'secondFriday', 'secondSaturday', 'secondSunday'
+    ];
+  
+    const dayIndex = Math.floor((date.getTime() - new Date('0001-01-01').getTime()) / (1000 * 60 * 60 * 24)) % 14;
+    return daysOfWeek[dayIndex];
+  }
+  
+  function timeTravel({ date, hour, minute, second }) {
+    const newDate = new Date(date);
+    newDate.setHours(hour);
+    newDate.setMinutes(minute);
+    newDate.setSeconds(second);
+    return newDate;
+  }
+//   console.log(addWeek(new Date('0001-01-01'))); // Output: Monday
+// console.log(addWeek(new Date('0001-01-02'))); // Output: Tuesday
+// console.log(addWeek(new Date('0001-01-07'))); // Output: Sunday
+// console.log(addWeek(new Date('0001-01-08'))); // Output: secondMonday
+// console.log(addWeek(new Date('0001-01-09'))); // Output: secondTuesday
 
-    const daysInNewWeek = 14
-    const dayofNewWeek = diffinDays % daysInNewWeek
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
-    'secondMonday', 'secondTuesday', 'secondWednesday', 'secondThursday', 'secondFriday',
-     'secondSaturday', 'secondSunday']
-    
-    return days[dayofNewWeek]
-}
-
-const date = new Date('0001-01-02')
-console.log(addWeek(date))
-
-
-function timeTravel({date, hour = 0, minute = 0 , second = 0}) {
-    const newDate = new Date(date)
-    newDate.setHours(hour, minute, second)
-    return newDate
-}
-
-const currentDate = new Date('2020-05-29 23:25:22')
-const modifiedDate = timeTravel({date: currentDate,hour: 21, minute: 22, second: 22}).toString()
-console.log(modifiedDate)
+// console.log(timeTravel({
+//   date: new Date('2020-05-29 23:25:22'),
+//   hour: 21,
+//   minute: 22,
+//   second: 22,
+// }).toString()); // Output: Fri May 29 2020 21:22:22 GMT+0100 (Western European Summer Time)
