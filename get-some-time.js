23ba902fb6e1b0deb2e065a0078beeae082228f3
viarony <victorpaularony>
2024-09-03 03:04:01 +0300

    FILE: to write the year in strind and weeks

--- get-some-time.js
+++ get-some-time.js
@@ -5,12 +5,4 @@ function firstDayWeek(week, year) {
     const firstDayOfWeek = new Date(firstDayOfYear.getTime() + (daysToAdd * 24 * 60 * 60 * 1000));
     return `${String(firstDayOfWeek.getDate()).padStart(2, '0')}-${String(firstDayOfWeek.getMonth() + 1).padStart(2, '0')}-${firstDayOfWeek.getFullYear()}`;
 }
-
-const tests = [
-    ({ eq }) => eq(firstDayWeek(1, '1000'), '01-01-1000'),
-    ({ eq }) => eq(firstDayWeek(52, '1000'), '22-12-1000'),
-    ({ eq }) => eq(firstDayWeek(2, '0001'), '08-01-0001'),
-    ({ eq }) => eq(firstDayWeek(43, '1983'), '17-10-1983'),
-    ({ eq }) => eq(firstDayWeek(23, '0091'), '04-06-0091'),
-    ({ eq }) => eq(firstDayWeek(2, '2017'), '02-01-2017')
-];
+console.log(firstDayWeek(1, '1000')); // Output: 01-01-2017
