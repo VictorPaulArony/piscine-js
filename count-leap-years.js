d2a29b4f523bd08a2e6576b37320cd041972d2df
viarony <victorpaularony>
2024-09-03 17:13:13 +0300

    FILE: function to check and count leap years

--- count-leap-years.js
+++ count-leap-years.js
@@ -2,12 +2,12 @@ function countLeapYears(date) {
     let year = date.getFullYear();
     let count = 0;
 
-    for (let i = 0; i <= year; i++) {
+    for (let i = 1; i < year; i++) {
         if (isLeapYear(new Date(i, 0, 1))) {
             count++;
         }
     }
-    return count -1 ;
+    return count;
 }
 
 function isLeapYear(date) {
@@ -16,5 +16,5 @@ function isLeapYear(date) {
 }
 
 // Example usage:
-console.log(countLeapYears(new Date('2020-01-01'))); // Output: 0
+console.log(countLeapYears(new Date('1664-08-09'))); // Output: 0
 console.log(countLeapYears(new Date('2048-12-08'))); // Example output: 490
