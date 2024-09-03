8a832c8ae6ed482225de2aea29b70e0d02e70952
viarony <victorpaularony>
2024-09-03 16:39:35 +0300

    FILE: function for time boolean

--- match-cron.js
+++ match-cron.js
@@ -1,28 +1,29 @@
-function matchCron(str, date) {
-    for (let i = 0; i < str.length; i++) {
-        if (i === 0 && (str[i] === '*' || parseInt(str[i]) <= 59)) {
+function matchCron(cronParts, date) {
+    // Check each part of the cron expression
+    for (let i = 0; i < cronParts.length; i++) {
+        if (i === 0) { // Minute
             let min = date.getMinutes();
-            if (str[i] !== '*' && min !== parseInt(str[i])) {
+            if (cronParts[i] !== '*' && min !== parseInt(cronParts[i])) {
                 return false;
             }
-        } else if (i === 1 && (str[i] === '*' || parseInt(str[i]) <= 23)) {
+        } else if (i === 1) { // Hour
             let hour = date.getHours();
-            if (str[i] !== '*' && hour !== parseInt(str[i])) {
+            if (cronParts[i] !== '*' && hour !== parseInt(cronParts[i])) {
                 return false;
             }
-        } else if (i === 2 && (str[i] === '*' || parseInt(str[i]) <= 31)) {
+        } else if (i === 2) { // Day of the Month
             let day = date.getDate();
-            if (str[i] !== '*' && day !== parseInt(str[i])) {
+            if (cronParts[i] !== '*' && day !== parseInt(cronParts[i])) {
                 return false;
             }
-        } else if (i === 3 && (str[i] === '*' || parseInt(str[i]) <= 12)) {
+        } else if (i === 3) { // Month
             let month = date.getMonth() + 1; // getMonth() returns 0-11
-            if (str[i] !== '*' && month !== parseInt(str[i])) {
+            if (cronParts[i] !== '*' && month !== parseInt(cronParts[i])) {
                 return false;
             }
-        } else if (i === 4 && (str[i] === '*' || parseInt(str[i]) <= 7)) {
+        } else if (i === 4) { // Day of the Week
             let dayOfWeek = date.getDay() || 7; // getDay() returns 0-6, we want 1-7
-            if (str[i] !== '*' && dayOfWeek !== parseInt(str[i])) {
+            if (cronParts[i] !== '*' && dayOfWeek !== parseInt(cronParts[i])) {
                 return false;
             }
         }
@@ -31,6 +32,6 @@ function matchCron(str, date) {
 }
 
 // Example usage:
-console.log(matchCron(['*', '*', '*', '*', '1'], new Date('2020-06-01 00:00:00'))); // true
+console.log(matchCron(['*', '3', '*', '*', '1'], new Date('2020-06-01 03:00:00'))); // true
 console.log(matchCron(['9', '*', '*', '*', '*'], new Date('2020-05-30 18:09:00'))); // true
-console
+console.log(matchCron(['9', '*', '*', '*', '*'], new Date('2020-05-30 19:21:00'))); // false
