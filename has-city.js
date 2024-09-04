c86d620515dd5433bb627f85b949ebce0b733594
viarony <victorpaularony>
2024-09-04 22:14:03 +0300

    FILE: function within a function

---
+++ has-city.js
@@ -0,0 +1,5 @@
+function hasCity(country, arr){
+    return function(city){
+        return arr.includes(city) ? `${city} is a city from ${country}` : `${city} is not a city from ${country}`
+    }
+}
