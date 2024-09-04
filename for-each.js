dd1d68298d4166c78ce4d076e0e280dd934a2f06
viarony <victorpaularony>
2024-09-04 19:50:30 +0300

    FILE: function that acts like the forEach method

--- for-each.js
+++ for-each.js
@@ -1,7 +1,12 @@
 function forEach(arr, func){
     for (let i = 0; i < arr.length; i++){
-        func(arr[i], i, arr)
+        func(arr[i],i, arr)
     }
-    return arr
 }
-// console.log(forEach([,2,3,4,5], (el) => console.log(el)))
+// console.log(forEach([1,2,3,4,5], (el) => console.log(el)))
+// Example usage
+// const numbers = [1, 2, 3, 4, 5];
+
+// forEach(numbers, (num, index) => {
+//     console.log(`Element at index ${index} is ${num}`);
+// });
