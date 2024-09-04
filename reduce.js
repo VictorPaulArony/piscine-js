b097614732d96f0fc4ece698e2874603b1f98bb2
viarony <victorpaularony>
2024-09-04 21:22:06 +0300

    FILE: functions that act like reduce and folt

---
+++ reduce.js
@@ -0,0 +1,37 @@
+function fold(arr, func, acc){
+    for(let i= 0; i< arr.length; i++){
+        acc = func(acc, arr[i], i, arr)
+    }
+    return acc
+}
+
+function foldRight(arr, func, acc){
+    for(let i = arr.length;i>= 0; i--){
+        acc = func(acc, arr[i], i, arr)
+    }
+    return acc
+}
+
+
+function reduce(arr,func){
+    let res = arr[0]
+    for (let i = 1; i < arr.length; i++){
+        res = func(res, arr[i], i, arr)
+    }
+    return res
+}
+
+function reduceRight(arr,func){
+    let res = arr[arr.length - 1]
+    for(let i = arr.length - 2; i >= 0; i--){
+        res = func(res, arr[i], i, arr)
+    }
+    return res
+}
+
+
+// const adder = (a, b) => a + b
+// fold([1, 2, 3], adder, 2) // returns 8 (2 + 1 + 2 + 3)
+// foldRight([1, 2, 3], adder, 2) // returns 8 (2 + 3 + 2 + 1)
+// reduce([1, 2, 3], adder) // returns 6 (1 + 2 + 3)
+// reduceRight([1, 2, 3], adder) // returns 6 (3 + 2 + 1)clea
