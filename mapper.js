747fa278d9084165b25f2d8efd40b499a4da3d47
viarony <victorpaularony>
2024-09-04 20:39:30 +0300

    FILE: function that acts like map method

--- mapper.js
+++ mapper.js
@@ -7,15 +7,11 @@ function map(arr, func){
 
 
 function flatMap(arr, func){
-    let res = [];
-    for(let i = 0;i < arr.length;i++){
-        res.push(func(arr[i],i,arr))
-    }
-    return res
+   return arr.reduce((acc,curr,i, arr) => acc.concat(func(curr,i,arr)),[])
 }
 // console.log(map([1,2,3,4], (num) => num * 2))
 // console.log(flatMap([1,2,3,4], (num) => [num, num * 2]))
 // const numbers = [10, -10, 20, -95, 86, 102, 35, 89, 110];
 // const add1 = (n) => n + 1;
 
-// console.log(map(numbers, add1)); 
+// console.log(flatMap(numbers, add1)); 
