a0c70a2da3be1338d42edea5c3e24ee848201ae8
viarony <victorpaularony>
2024-09-07 02:05:18 +0300

    file: function to build blocks

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,37 +1,38 @@
-export function build(){
-    let body = document.getElementById("body")[0]
-    let bricks = 1
+export function build(n) {
+    let body = document.body;
+    let bricks = 1;
     let inter = setInterval(() => {
-        if (bricks > n){
-            clearInterval(inter)
-            return
+        if (bricks > n) {
+            clearInterval(inter);
+            return;
         }
-        bricks++
-        let brk = document.createElement("div")
-        brk.setAttribute("id", "brick-" + bricks)
-        if (bricks%3 === 2){_
-        brk.dataset.foundation = true
+
+        let brk = document.createElement("div");
+        brk.setAttribute("id", "brick-" + bricks);
+        if (bricks % 3 === 2) {
+            brk.dataset.foundation = true;
         }
-        body.appendChild(brk)
-       
-    }, 100)
-}
-export function destroy(...ids){
-    ids.forEach((id) => {
-        let brick = document.getElementById(id)
-        if (brick){
-            if(brick.dataset.foundation){
-                brick.dataset.repaired = "in progress"
-            }else {
-                brick.dataset.repaired = "true"
-            }
-        }
-    })
+        body.appendChild(brk);
+        bricks++;
+    }, 100);
 }
 
-export function destroy(){
-    let bricks = document.getElementById("div")
-    if (bricks.length >0){
-        bricks[bricks.length-1].remove()
+export function destroy(ids) {
+    Object.values(ids).forEach((id) => {
+        let brick = document.getElementById(id);
+        if (brick) {
+            if (brick.dataset.foundation) {
+                brick.dataset.repaired = "in progress";
+            } else {
+                brick.dataset.repaired = "true";
+            }
+        }
+    });
+}
+
+export function destroyOne() {
+    let bricks = document.querySelectorAll('div');
+    if (bricks.length > 0) {
+        bricks[bricks.length - 1].remove();
     }
 }
