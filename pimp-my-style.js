2fa1fc04758a28825dc9643b7669908cb3fc5ef1
viarony <victorpaularony>
2024-09-07 11:04:31 +0300

    FILE: function to use classList method to add toggle and remove

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,22 +1,19 @@
 import { styles } from "./pimp-my-style.data.js"
-const count = 0
+let count = 0
 
 export function pimp(){
-    const button = document.querySelector("button.button")
-    if (!button.classList.contains("unpimp")){
+    const button = document.querySelector(".button")
+    if (count < styles.length){
         button.classList.add(styles[count])
         count++
-        if(count === styles.length){
-            button.classList.add("unpimp")
-        }
+        button.classList.remove("unpimp")
     }else {
         count--
         button.classList.remove(styles[count])
-        if (count === 0){
-            button.classList.toggle("unpimp")
-        }
+        button.classList.add("unpimp")
+        
     }
-    // if(count === styles.length){
-    //     button.classList.toggle("unpimp")
-    // }
+    if(count === 0){
+        button.classList.remove("unpimp")
+    }
 }
