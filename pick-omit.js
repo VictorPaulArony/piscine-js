ccbfece76cf52c2ee0745e9b34d396a38c0e0cb2
viarony <victorpaularony>
2024-09-10 16:16:42 +0300

    FILE: function to pick

--- pick-omit.js
+++ pick-omit.js
@@ -3,3 +3,18 @@ function pick(obj, arr){
     _.some(arr, str => _.includes(key, str)))
     return match
 }
+
+function omit(obj, keys) {
+    // Ensure keys is an array
+    if (typeof keys === 'string') {
+        keys = [keys];
+    }
+
+    const result = {};
+    for (const key in obj) {
+        if (!keys.includes(key)) {
+            result[key] = obj[key];
+        }
+    }
+    return result;
+}
