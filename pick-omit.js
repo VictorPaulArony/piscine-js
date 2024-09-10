2c874c81651d87324169deac88a9094c41c42876
viarony <victorpaularony>
2024-09-10 17:31:43 +0300

    FILE: function to pick

--- pick-omit.js
+++ pick-omit.js
@@ -1,28 +1,22 @@
-function pick(obj, keys){
-    if (typeof keys === 'string') {
-        keys = [keys];
-    }
-
-    const result = {};
-    for (const key in obj) {
-        if (keys.hasOwnProperty(key)) {
-            result[key] = obj[key];
-        }
-    }
-    return result;
-}
-
-function omit(obj, keys) {
-    // Ensure keys is an array
-    if (typeof keys === 'string') {
-        keys = [keys];
-    }
-
-    const result = {};
-    for (const key in obj) {
-        if (!keys.hasOwnProperty(key)) {
-            result[key] = obj[key];
-        }
-    }
-    return result;
-}
+function pick(obj, str) {
+    let keys = Object.keys(obj);
+    let output = {};
+    let pic = keys.filter((key) =>
+      typeof str === "string" ? key === str : str.includes(key)
+    );
+    pic.forEach((element) => {
+      output[element] = obj[element];
+    });
+    return output;
+  }
+  function omit(obj, str) {
+    let keys = Object.keys(obj);
+    let output = {};
+    let pic = keys.filter((key) =>
+      typeof str === "string" ? key !== str : !str.includes(key)
+    );
+    pic.forEach((element) => {
+      output[element] = obj[element];
+    });
+    return output;
+  }
