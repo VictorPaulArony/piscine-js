6784e1a122566a61fa4e8f540f4630657a04bd5f
viarony <victorpaularony>
2024-09-11 23:51:51 +0300

    FUNCTION: that deep objects ar array

--- deep-copy.js
+++ deep-copy.js
@@ -1,3 +1,22 @@
-function deepCopy (obj){
-    return JSON.parse(JSON.stringify(obj))
-}
+// function deepCopy (obj){
+//     return JSON.parse(JSON.stringify(obj))
+// }
+
+function deepCopy(original) {
+    if (Array.isArray(original)) {
+      const copy = [];
+      for (const [index, value] of original.entries()) {
+        copy[index] = deepCopy(value);
+      }
+      return copy;
+    } else if (typeof original === 'object' && original !== null) {
+      const copy = {};
+      for (const [key, value] of Object.entries(original)) {
+        copy[key] = deepCopy(value);
+      }
+      return copy;
+    } else {
+      // Primitive value: atomic, no need to copy
+      return original;
+    }
+  }
