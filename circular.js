87b6b9268fb778e50b28767ae5b8305232aeac1c
viarony <victorpaularony>
2024-09-12 11:40:25 +0300

    UPDATE: creating an object

--- circular.js
+++ circular.js
@@ -1,6 +1,10 @@
-const circular = {
-    circular: null
+// const circular = {
+//     circular: null
+// }
+// circular.circular = circular
+
+// console.log(circular)
+const circular ={
 }
 circular.circular = circular
-
 console.log(circular)
