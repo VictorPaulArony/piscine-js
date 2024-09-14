c53640a4f1a4f6eba9485f6b69d57cbfe5392c16
Victor  Arony <viarony@noreply.learn.zone01kisumu.ke>
2024-09-14 17:51:16 +0000

    Fuction that act like the debounce method

---
+++ debounce.js
@@ -0,0 +1,36 @@
+function debounce(func, wait) {
+  let timeout;
+
+  return function (...args) {
+    const context = this;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      func(...args);
+    }, wait);
+  };
+}
+
+function opDebounce(fn, delay, options) {
+  var timer = null,
+    first = true,
+    leading;
+  if (typeof options === "object") {
+    leading = !!options.leading;
+  }
+  return function () {
+    let context = this,
+      args = arguments;
+    if (first && leading) {
+      fn.apply(context, args);
+      first = false;
+    }
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  };
+}
