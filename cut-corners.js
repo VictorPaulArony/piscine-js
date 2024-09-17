2deb3107edeb11447e5653ce6f88174a4cb79ed3
viarony <victorpaularony>
2024-09-17 10:50:53 +0300

    UPDATED: cutconers with new implementation

--- cut-corners.js
+++ cut-corners.js
@@ -1,55 +1,53 @@
+const round = (num) =>{
+  let sign = num >= 0 ? 1 : -1 
+  let res = 0
+  let n = finder(Math.abs(num))
+  let rem = Math.abs(num)- n
+  if(rem >= 0.5){
+      res = n + 1
+  }else{
+      res = n
+  }
+  return res *sign
+}
+const ceil = (num) => {
+  let sign = num >= 0 ? 1 : -1
+  let res = 0
+  let n = finder(Math.abs(num))
+  let rem = num - n
+  if (rem > 0){
+      res = n + 1
+  }else{
+      res = n
+  }
+  return res * sign
+}
+function floor(num){
+  const whole = finder(num);
+    return num < whole ? whole - 1 : whole;
+}
+function trunc(num){
+  return num >= 0 ? finder(num) : -finder(-num)
+}
 
-function round(num) {
-      const integerPart = getIntegerPart(num)
-      const decimalPart = num - integerPart
-      if (Math.abs(decimalPart) >= 0.5) {
-        return integerPart + (num >= 0 ? 1 : -1)
+function finder(num){
+  let sign = num >= 0 ? 1 : -1 
+  let res = 0
+  let i = 0
+  let numStr = Math.abs(num) + ''
+  while (numStr.length > i){
+      if (num[i] !== '.'){
+          res = res *10 + (numStr[i] - '0')
+          break
       }
-    return integerPart
   }
+  // console.log(res * sign )
+  return res * sign
   
-  function ceil(num) {
-    if (num >= 0) {
-      const integerPart = getIntegerPart(num)
-      return num > integerPart ? integerPart + 1 : integerPart;
-    } else {
-      return -getIntegerPart(-num);
-    }
-  }
-  
-  function floor(num) {
-      const integerPart = getIntegerPart(num);
-      return num < integerPart ? integerPart - 1 : integerPart;
-  }
-  
-  function trunc(num) {
-    return num >= 0 ? getIntegerPart(num) : -getIntegerPart(-num);
-  }
-  
-  function getIntegerPart(num) {
-    let sign = num >= 0 ? 1 : -1
-    num = Math.abs(num)
-    var res = 0
-    var pla = 1
-    while(pla <= num){
-        pla *= 10
-    }
-    pla /= 10
-    while(pla >= 1){
-       while(res + pla <= num){
-        res += pla
-       }
-       pla /= 10
-    }
-    return res * sign
-  }
-  
- 
-  
-  
-  // const nums =  [3, -3, 3, -3, 0];
-  // console.log(nums.map(round)); // Expected: [3, -3, 3, -3, 0]
-  // console.log(nums.map(floor)); // Expected: [3, -4, 2, -3, 0]
-  // console.log(nums.map(trunc)); // Expected: [3, -3, 2, -2, 0]
-  // console.log(nums.map(ceil));  // Expected: [4, -3, 3, -2, 0]
-  
+}
+
+// const nums = [3.7, -3.7, 3.1, -3.1]
+// console.log(nums.map(round))//[ 4, -4, 3, -3 ]
+// console.log(nums.map(ceil))//[ 4, -3, 4, -3 ]
+// console.log(nums.map(floor))//[ 3, -4, 3, -4 ]
+// console.log(nums.map(trunc))//[ 3, -3, 3, -3 ]
---
+++ prac.js
@@ -0,0 +1,53 @@
+const round = (num) =>{
+    let sign = num >= 0 ? 1 : -1 
+    let res = 0
+    let n = finder(Math.abs(num))
+    let rem = Math.abs(num)- n
+    if(rem >= 0.5){
+        res = n + 1
+    }else{
+        res = n
+    }
+    return res *sign
+}
+const ceil = (num) => {
+    let sign = num >= 0 ? 1 : -1
+    let res = 0
+    let n = finder(Math.abs(num))
+    let rem = num - n
+    if (rem > 0){
+        res = n + 1
+    }else{
+        res = n
+    }
+    return res * sign
+}
+function floor(num){
+    const whole = finder(num);
+      return num < whole ? whole - 1 : whole;
+}
+function trunc(num){
+    return num >= 0 ? finder(num) : -finder(-num)
+}
+
+function finder(num){
+    let sign = num >= 0 ? 1 : -1 
+    let res = 0
+    let i = 0
+    let numStr = Math.abs(num) + ''
+    while (numStr.length > i){
+        if (num[i] !== '.'){
+            res = res *10 + (numStr[i] - '0')
+            break
+        }
+    }
+    // console.log(res * sign )
+    return res * sign
+    
+}
+
+// const nums = [3.7, -3.7, 3.1, -3.1]
+// console.log(nums.map(round))//[ 4, -4, 3, -3 ]
+// console.log(nums.map(ceil))//[ 4, -3, 4, -3 ]
+// console.log(nums.map(floor))//[ 3, -4, 3, -4 ]
+// console.log(nums.map(trunc))//[ 3, -3, 3, -3 ]
