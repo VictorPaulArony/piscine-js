30ab38b1814b926d426e0372ec1c2a2910100a17
viarony <victorpaularony>
2024-09-18 15:25:10 +0300

    FILE: get element by tag name

--- prac.js
+++ prac.js
@@ -1,13 +1,25 @@
-let n = 5;
+export const getArchitects = () =>{
+    const archs = Array.from(document.querySelectorAll("classical"))
+    // const archs = Array.
+    const  nonArchs = Array.from(document.querySelectorAll("*:not(classical)"))
+    return [archs, nonArchs]
+}
+export const getClassical = () => {
+    const [archs, _] = getArchitects()
+    const classical = Array.filter(archs => archs.classList.contains("classial"))
+    const nonClas = Array.filter(archs => !archs.classList.contains("classical"))
+    return[classical, nonClas]
+}
+export const getActive = () =>{
+    const [classical, _] = getActive()
+    const act = Array.filter(classical => classical.classList.contains("active") )
+    const inact = Array.filter(classical => !classical.classList.contains("active") )
 
-for (let i = 1; i <= n; i++) {
-  // printing spaces
-  for (let j = 0; j < n - i; j++) {
-    process.stdout.write(' ');
-  }
-  // printing star
-  for (let k = 0; k < i*2-1 ; k++) {
-    process.stdout.write('#');
-  }
-  console.log();
+    return [act, inact]
+}
+export function getBonannoPisano(){
+    const [act, _] = getActive()
+    const bonanno = Array.filter(act => act.classList.contains("BonannoPisano"))
+    // const bon = Array.filter(act => !act.classList.contains("BonannoPisano"))
+    return[bonanno]
 }
